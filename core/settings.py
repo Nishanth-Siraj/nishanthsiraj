b0117f10fbfdc588680ca4832da5491682bc4c24
developernishanthsiraj <nishanth@chadura.com>
2025-09-11 11:57:44 +0530

    static file set

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from os.path import join, dirname
+from datetime import timedelta
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE = dirname(BASE_DIR)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,9 +33,13 @@ SECRET_KEY = "django-insecure-(t3u!x05qh@kaz_0sv%w=*#-m4_amdq_^o767pz0s@kl@jq$9u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-
+STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE, "static")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE, "media")
 # Application definition
 
 INSTALLED_APPS = [
